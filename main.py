be14c7fe5f1b2999768a0eb5f3a91ed06b83b323
DATAGENIC\anupam.soni <anupam.soni@datagenicgroup.com>
2018-08-10 12:37:51 +0100

    main updated

--- main.py
+++ main.py
@@ -5,24 +5,29 @@ df = pd.read_csv("PLATTS GI & GJ - GDATAL-5973.csv")
 temp_list = list(df["SYMBOL"])
 temp_list2 = []
 for symbol in temp_list:
-    print("Seaching : "+ symbol)
-    driver = webdriver.Chrome("C:\\Python36-32\\selenium\\webdriver\\chromedriver.exe")
-    url= "https://www.spglobal.com/platts/en/our-methodology/symbol-page-directories"
-    driver.maximize_window()
-    driver.get(url)
-    search = driver.find_element_by_id("txtSymbol")
-    search.send_keys(symbol)
-    driver.find_element_by_id("btnSymbol").click()
-    time.sleep(1)
-    driver.find_element_by_xpath("""//*[@id="symbolResult"]/div/ul/li/a""").click()
-    # print(driver.find_element_by_xpath("""//*[@id="5tnhj0-acc-menu"]/li[11]/div[2]/h5""").text)
-    start_date = driver.find_element_by_xpath("""//*[@id="5tnhj0-acc-menu"]/li[11]/div[2]/h5""").text
-    # print(driver.find_element_by_xpath("""//*[@id="5tnhj0-acc-menu"]/li[12]/div[2]/h5""").text)
-    end_date = driver.find_element_by_xpath("""//*[@id="5tnhj0-acc-menu"]/li[12]/div[2]/h5""").text
-    row  = symbol,start_date,end_date
-    print(row)
-    temp_list2.append(row)
-    driver.close()
+    start_date =""
+    end_date = ""
+    try:
+        print("Seaching : "+ symbol)
+        driver = webdriver.Chrome("C:\\Python36-32\\selenium\\webdriver\\chromedriver.exe")
+        url= "https://www.spglobal.com/platts/en/our-methodology/symbol-page-directories"
+        driver.maximize_window()
+        driver.get(url)
+        search = driver.find_element_by_id("txtSymbol")
+        search.send_keys(symbol)
+        driver.find_element_by_id("btnSymbol").click()
+        time.sleep(1)
+        driver.find_element_by_xpath("""//*[@id="symbolResult"]/div/ul/li/a""").click()
+        # print(driver.find_element_by_xpath("""//*[@id="5tnhj0-acc-menu"]/li[11]/div[2]/h5""").text)
+        start_date = driver.find_element_by_xpath("""//*[@id="5tnhj0-acc-menu"]/li[11]/div[2]/h5""").text
+        # print(driver.find_element_by_xpath("""//*[@id="5tnhj0-acc-menu"]/li[12]/div[2]/h5""").text)
+        end_date = driver.find_element_by_xpath("""//*[@id="5tnhj0-acc-menu"]/li[12]/div[2]/h5""").text
+        row  = symbol,start_date,end_date
+        print(row)
+        temp_list2.append(row)
+        driver.close()
+    except:
+        pass
 
     # mySelect = Select(driver.find_element_by_xpath("""//*[@id="txtSymbol"]"""))
     # print(mySelect)
